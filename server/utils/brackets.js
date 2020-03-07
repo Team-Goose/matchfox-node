208c5a9076bde8dab1980e97c9be27bb049eeb27
Reetik Patel <enceledus@esfera-triumph.ddns.uark.edu>
2020-03-06 20:06:17 -0600

    :construction:Initialize bracket generator function

---
+++ server/utils/brackets.js
@@ -0,0 +1,14 @@
+
+var Brackets = function() {
+    var self = {};
+
+    self.handler = {};
+
+    function generateBracket(users) {
+        
+    }
+
+    return self;
+}
+
+module.exports = Brackets();
