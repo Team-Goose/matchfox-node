4dec73a794a6806069b569d7d096d03222fec4c7
Reetik Patel <enceledus@esfera-triumph.ddns.uark.edu>
2020-03-07 02:27:40 -0600

    :wrench:Refactored firebaseUtils

--- functions/utils/firebaseUtils.js
+++ functions/utils/firebaseUtils.js
@@ -1,39 +1,10 @@
 var admin = require('firebase-admin');
 
-var firebaseUtils = function() {
-    var self = {};
+var serviceAccount = require("../secrets/serviceAccountKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://matchfoxdb.firebaseio.com"
+});
 
-    var serviceAccount = require("../secrets/serviceAccountKey.json");
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "https://matchfoxdb.firebaseio.com"
-    });
+modules.export = admin.firestore();
     
-    let db = admin.firestore();
-    
-    /**
-     * Gets single data object from firebase
-     * @param {string} collection
-     * @param {string} document
-     * @returns {Promise}
-     */
-    self.getSingleData = function(collection, document) {
-        return new Promise(function(resolve, reject) {
-            var docRef = db.collection(collection).doc(document);
-            docRef.get().then(doc => {
-                if(doc.exists) {
-                    resolve(doc.data());
-                } else {
-                    resolve({});
-                }
-            }).catch(err => {
-                console.error(err);
-                reject(err);
-            });
-        });
-    }
-    
-    return self;
-}
-
-module.exports = firebaseUtils();
