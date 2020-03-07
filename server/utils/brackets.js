1a32b80d0e2dd8dbecd139f833ec2aff7acafb57
Reetik Patel <enceledus@esfera-triumph.ddns.uark.edu>
2020-03-06 20:12:34 -0600

    :construction:Added comments to generate bracket

--- server/utils/brackets.js
+++ server/utils/brackets.js
@@ -4,9 +4,25 @@ var Brackets = function() {
 
     self.handler = {};
 
+    /**
+     * Generates a bracket from an array of user objects
+     * @param {Object[]} users
+     * @param {string} users[].user_id
+     * @param {number} users[].seed
+     * @returns {Object} bracket
+     */
     function generateBracket(users) {
+        // var users = [
+        //     {
+        //         user_id: "adbeo23",
+        //         seed: 1500
+        //     }
+        // ];
+    
+        // users[0].seed = 103;
         
     }
+
 
     return self;
 }
