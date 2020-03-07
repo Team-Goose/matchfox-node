da2a3a8bc95c9cb4d4160c0329868201302ad069
Reetik Patel <enceledus@esfera-triumph.ddns.uark.edu>
2020-03-06 22:16:47 -0600

    :wrench:Firebase configs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const firebase = ('firebase/app');
 
 const app = express();
 const port = 3000;
@@ -9,3 +10,15 @@ app.use(express.static('public'));
 
 
 app.listen(port, () => console.log(`Matchfox listening on port ${port}`));
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDHmk3WrWG-yVP1b0BH0Lw6fIQXZ3vEDeU",
+    authDomain: "matchfoxdb.firebaseapp.com",
+    databaseURL: "https://matchfoxdb.firebaseio.com",
+    projectId: "matchfoxdb",
+    storageBucket: "matchfoxdb.appspot.com",
+    messagingSenderId: "874376032766",
+    appId: "1:874376032766:web:3b2a1472dc0cc12d5910af",
+    measurementId: "G-8KGEJ570FV"
+});
