5bb501293a62c0904a2405c1914a7520f3e6ca72
Reetik Patel <enceledus@Esfera-Triumph-2.local>
2020-10-27 21:11:01 -0500

    🔒 Implemented secret decryption using runtime param

--- functions/helpers/secrets.js
+++ functions/helpers/secrets.js
@@ -1,8 +1,14 @@
 var CryptoJS = require('crypto-js');
+var argv = require('minimist')(process.argv.slice(2));
+
 const secFil = require("../secrets.json");
 
 var Secrets = function() {
     this.get = function(key) {
+        var encrypted = secFil[key];
+        return this.decrypt(encrypted, argv.crypt);
+    }
+
     this.encrypt = function(secret, key) {
         return CryptoJS.AES.encrypt(secret, key);
     }
