2b02031c8fea56464592c90e4e322264f843b465
Reetik Patel <enceledus@Esfera-Triumph-2.local>
2020-10-27 21:09:44 -0500

    🚧🔒 Implemented encryption for later

--- functions/helpers/secrets.js
+++ functions/helpers/secrets.js
@@ -3,6 +3,10 @@ const secFil = require("../secrets.json");
 
 var Secrets = function() {
     this.get = function(key) {
+    this.encrypt = function(secret, key) {
+        return CryptoJS.AES.encrypt(secret, key);
+    }
+
     this.decrypt = function(secret, key) {
         return CryptoJS.AES.decrypt(secret, key);
     }
